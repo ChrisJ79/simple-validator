5c2a78ef7467db2a291aec7bacf4d458279ba740
Chris <csjohnson79@icloud.com>
2016-09-26 22:16:21 +0000

    Hopefully this first commit works...

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+
+// var phoneNum = prompt("Please enter your phone number (Be sure to use dashes after 3rd and 6th numbers): ");
+// alert(phoneNum.charAt(3) === '-' && phoneNum.charAt(7) === '-' && phoneNum.length === 12);
+
+
+// var birthDate = prompt("Please enter your phone number (Be sure to use dashes after 3rd and 6th numbers): ");
+// alert(birthDate.charAt(2) === '/' && birthDate.charAt(5) === '/' && birthDate.length === 8);
+
+// var postalCode = prompt("Please enter your postal code (Must be 5 digits): ");
+// alert(postalCode.length === 5);
+
+// var stateAbbrv = prompt("Please enter your 2 digit State Abbreviation: ");
+// alert(stateAbbrv.length === 2 && stateAbbrv ===stateAbbrv.toUpperCase());
+
+var yesNo = prompt("You stupid or sumthin (Answer yes or no)?");
+if (yesNo === "yes" ) {
+    document.write("yes")
+} else {
+    document.write("no")
+};
+
